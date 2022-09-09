f42de3736c2764eb5c5a2170a1eee7690d3583e8
Foweraind <98493067+Foweraind@users.noreply.github.com>
2022-09-09 13:37:34 -0300

    Blog Foweraind V0.36

--- js/função.js
+++ js/função.js
@@ -9,17 +9,12 @@ const menulist = document.querySelector('.menu')
 /*função do menu hamburguer*/
 
 menu.addEventListener('click', () => {
-    if (menulist.style.display == 'flex') {
-        menulist.style.display = 'none'
-    } else {
-        menulist.style.display = 'flex'
-    }
+    menulist.classList.toggle('active')
 })
 
 function changedSize() {
     if (window.innerWidth >= 769) {
-        menulist.style.display = 'flex'
+        menulist.classList.remove('active')
     } else {
-        menulist.style.display = 'none'
     }
 }
